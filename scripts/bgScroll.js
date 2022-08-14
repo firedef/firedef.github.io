629c39ef38f510a25c7132188a27711c2f6d9753
firedef <firedef2019@gmail.com>
2022-08-14 18:34:43 +0300

    Update style

--- scripts/bgScroll.js
+++ scripts/bgScroll.js
@@ -6,7 +6,17 @@ function bgScroll() {
       var parallax = window.getComputedStyle(reveals[i]).getPropertyValue("--parallax");
       var offset = 100 -Math.max(Math.min((elementTop - windowHeight) * -0.01 * parallax, 100), 0);
       
-      reveals[i].style.backgroundPositionY = offset + "%";
+      reveals[i].style.setProperty("background-position-y", offset + "%");
+    }
+
+    var reveals = document.querySelectorAll(".scroll-bg-fixed");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = document.documentElement.scrollTop;
+      var parallax = window.getComputedStyle(reveals[i]).getPropertyValue("--parallax");
+      var offset = 100 -Math.max(Math.min((elementTop - windowHeight) * -0.01 * parallax, 100), 0);
+      
+      reveals[i].style.setProperty("background-position-y", offset + "%");
     }
 
     reveals = document.querySelectorAll(".scroll-element");
@@ -14,7 +24,7 @@ function bgScroll() {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var parallax = window.getComputedStyle(reveals[i]).getPropertyValue("--parallax");
-      var offset = Math.max(Math.min((elementTop - windowHeight * .5) * -0.01 * parallax, 20), -20);
+      var offset = Math.max(Math.min((elementTop - windowHeight * 0.5) * -0.01 * parallax, 40), -40);
       
       reveals[i].style.transform = "translateY(" + offset + "%)";
     }
